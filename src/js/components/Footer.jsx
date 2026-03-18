14d750664eaded3ff13b091da3c90cddcf286b8c
Ana Lopez <avantadesignlab@gmail.com>
2026-03-18 12:48:52 +0000

    style: add padding to footer

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="container-fluid bg-dark d-flex align-items-center justify-content-center p-2">
+        <div className="container-fluid bg-dark d-flex align-items-center justify-content-center p-3">
             <p className="text-light m-0">
                 Copyright ® avantadesignlab.com
             </p>
